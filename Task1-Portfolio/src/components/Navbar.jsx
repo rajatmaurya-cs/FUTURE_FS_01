0e8fb2792c81648c96a47b9629f86c7d9d353abc
Rajat Maurya <rajatmaurya176@gmail.com>
2026-03-17 21:23:01 +0530

    fix: building error

--- Task1-Portfolio/src/components/Navbar.jsx
+++ Task1-Portfolio/src/components/Navbar.jsx
@@ -23,6 +23,7 @@ function NavBar() {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsMobileMenuOpen(false);
   }, [location.pathname]);
 
